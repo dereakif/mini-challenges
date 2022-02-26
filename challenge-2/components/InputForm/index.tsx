6e1fa39f8597687ab870dae123c49abe03fd16e4
Akif <dereakif@gmail.com>
2022-02-26 23:12:21 +0300

    done: form validation

--- challenge-2/components/InputForm/index.tsx
+++ challenge-2/components/InputForm/index.tsx
@@ -1,4 +1,4 @@
-import { Dispatch, FormEvent, SetStateAction } from "react";
+import { ChangeEvent, Dispatch, FormEvent, SetStateAction } from "react";
 import { UserInfo } from "../../interfaces/user.interfaces";
 import styles from "../../styles/InputForm.module.scss";
 
@@ -6,15 +6,81 @@ type Props = {
   input: UserInfo;
   setInput: Dispatch<SetStateAction<UserInfo>>;
   setUsers: Dispatch<SetStateAction<UserInfo[]>>;
+  errors: UserInfo;
+  setErrors: Dispatch<SetStateAction<UserInfo>>;
 };
 
-function InputForm({ input, setInput, setUsers }: Props) {
+function InputForm({ input, setInput, setUsers, errors, setErrors }: Props) {
+  const validatePhoneNumber = (phoneNumber: string) => {
+    let validity = true;
+    let error = "";
+    if (phoneNumber.length !== 10) {
+      validity = false;
+      error = "Telefon numarası 10 haneli olmalıdır.";
+    }
+    return { validity, error };
+  };
+
+  const validateName = (name: string) => {
+    let validity = true;
+    let error = "";
+
+    if (name.trim() === "") {
+      validity = false;
+      error = "Bu alan boş bırakılamaz.";
+      return { validity, error };
+    }
+    if (!name.match(/^[a-zA-Z]+$/)) {
+      validity = false;
+      error = "Bu alan sadece harflerden oluşmalıdır.";
+      return { validity, error };
+    }
+    return { validity, error };
+  };
+
+  const validateForm = () => {
+    let formIsValid = true;
+    Object.keys(input).forEach((key) => {
+      if (key === "phone") {
+        const { validity, error } = validatePhoneNumber(input[key]);
+        if (!validity) {
+          formIsValid = false;
+          setErrors((prevState) => ({
+            ...prevState,
+            phone: error,
+          }));
+        } else {
+          setErrors((prevState) => ({
+            ...prevState,
+            phone: "",
+          }));
+        }
+      } else {
+        const { validity, error } = validateName(input[key as keyof UserInfo]);
+        if (!validity) {
+          formIsValid = false;
+          setErrors((prevState) => ({ ...prevState, [key]: error }));
+        } else {
+          setErrors((prevState) => ({ ...prevState, [key]: "" }));
+        }
+      }
+    });
+    return formIsValid;
+  };
+
   const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    console.log("submit");
-    setUsers((s) => [...s, input]);
-    setInput({ firstName: "", lastName: "", phone: "" });
+    if (validateForm()) {
+      setUsers((s) => [...s, input]);
+      setInput({ firstName: "", lastName: "", phone: "" });
+    }
   };
+
+  const checkInput = (e: ChangeEvent<HTMLInputElement>) => {
+    const onlyDigits = e.target.value.replace(/\D/g, "");
+    setInput({ ...input, phone: onlyDigits });
+  };
+
   return (
     <form className={styles.form} onSubmit={handleSubmit}>
       <div className="form-group">
@@ -25,6 +91,7 @@ function InputForm({ input, setInput, setUsers }: Props) {
           value={input.firstName}
           onChange={(e) => setInput({ ...input, firstName: e.target.value })}
         />
+        {errors.firstName && <span>{errors.firstName}</span>}
       </div>
       <div className="form-group">
         <label>Soyad:</label>
@@ -34,6 +101,7 @@ function InputForm({ input, setInput, setUsers }: Props) {
           value={input.lastName}
           onChange={(e) => setInput({ ...input, lastName: e.target.value })}
         />
+        {errors.lastName && <span>{errors.lastName}</span>}
       </div>
       <div className="form-group">
         <label>Telefon:</label>
@@ -41,8 +109,10 @@ function InputForm({ input, setInput, setUsers }: Props) {
           type="text"
           name="phone"
           value={input.phone}
-          onChange={(e) => setInput({ ...input, phone: e.target.value })}
+          maxLength={10}
+          onChange={checkInput}
         />
+        {errors.phone && <span>{errors.phone}</span>}
       </div>
       <button className={styles.greenBtn} type="submit">
         Ekle
